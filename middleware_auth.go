e834a34cabac70286e751ce1dabaf29687aacd0f
Dominik Feret <dominik.feret03@gmail.com>
2024-02-16 14:16:28 +0100

    Create middleware authentication

--- middleware_auth.go
+++ middleware_auth.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/DominikFeret/rss-aggregator/internal/auth"
+	"github.com/DominikFeret/rss-aggregator/internal/database"
+)
+
+type authedHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func (apiCfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetAPIKey(r.Header)
+		if err != nil {
+			respondWithError(w, http.StatusUnauthorized, fmt.Sprint("Error getting API key: ", err))
+			return
+		}
+
+		user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, fmt.Sprint("Error getting user: ", err))
+			return
+		}
+
+		handler(w, r, user)
+	}
+}
