938c0270be6449fb8091a15441a7412a3ba26d40
Dominik Feret <dominik.feret03@gmail.com>
2024-02-19 14:08:53 +0100

    Add Post model and SQL queries for creating posts

--- scraper.go
+++ scraper.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/DominikFeret/rss-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(db *database.Queries, concurrency int, timeInterval time.Duration) {
@@ -47,7 +50,34 @@ func scrapeFeed(db *database.Queries, feed database.Feed, wg *sync.WaitGroup) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Printf("Scraped %v from %v\n", item.Title, feed.Url)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubDate, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			log.Println("Error parsing date: ", err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubDate,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("Error creating post: ", err)
+		}
 	}
 	log.Printf("Scraped %v posts from %v\n", len(rssFeed.Channel.Item), feed.Url)
 }
